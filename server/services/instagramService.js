b0fc3314c9cacebd8450bfc323a38718e8884b30
codewithmayor <mayor.michael19@gmail.com>
2025-05-27 22:23:20 +0200

    updated igservice file

--- server/services/instagramService.js
+++ server/services/instagramService.js
@@ -1,38 +1,59 @@
 // server/services/instagramService.js
-const axios = require('axios');
+const axios = require("axios");
 const PAGE_TOKEN = process.env.INSTAGRAM_ACCESS_TOKEN;
-const API_URL    = 'https://graph.facebook.com/v16.0/me/messages';
+const API_URL = "https://graph.facebook.com/v16.0/me/messages";
 
 async function sendText(recipientId, text) {
-  await axios.post(API_URL, {
-    recipient: { id: recipientId },
-    message:   { text }
-  }, { params: { access_token: PAGE_TOKEN }});
+  await axios.post(
+    API_URL,
+    {
+      recipient: { id: recipientId },
+      message: { text },
+    },
+    { params: { access_token: PAGE_TOKEN } }
+  );
 }
 
 async function sendQuickReplies(recipientId, text, replies) {
-  const qr = replies.map(title => ({ content_type: 'text', title, payload: title }));
-  await axios.post(API_URL, {
-    recipient: { id: recipientId },
-    message:   { text, quick_replies: qr }
-  }, { params: { access_token: PAGE_TOKEN }});
+  const qr = replies.map((title) => ({
+    content_type: "text",
+    title,
+    payload: title,
+  }));
+  try {
+    await axios.post(
+      API_URL,
+      {
+        recipient: { id: recipientId },
+        message: { text, quick_replies: qr },
+      },
+      { params: { access_token: PAGE_TOKEN } }
+    );
+  } catch (err) {
+    console.error("Instagram API error:", err.response?.data || err.message);
+    throw err;
+  }
 }
 
 async function sendButtonTemplate(recipientId, text, buttons) {
   // buttons: [ { type:'web_url', url, title }, { type:'postback', title, payload } ]
-  await axios.post(API_URL, {
-    recipient: { id: recipientId },
-    message: {
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'button',
-          text,
-          buttons
-        }
-      }
-    }
-  }, { params: { access_token: PAGE_TOKEN }});
+  await axios.post(
+    API_URL,
+    {
+      recipient: { id: recipientId },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "button",
+            text,
+            buttons,
+          },
+        },
+      },
+    },
+    { params: { access_token: PAGE_TOKEN } }
+  );
 }
 
 module.exports = { sendText, sendQuickReplies, sendButtonTemplate };
